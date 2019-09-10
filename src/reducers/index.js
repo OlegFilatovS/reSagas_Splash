d1cd109174a94a68e071b61a6bfd68f7e4219a1e
OlegF <oleg.filatov@gs-labs.ru>
2019-09-10 22:22:49 +0300

    some

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,14 @@
 import errorReducer from "./errorReducer";
 import imagesReducers from "./imagesReducer";
 import loadingReducer from "./loadingReducer";
+import pageReducer from "./pageReducer";
 import {combineReducers} from "redux";
 
 const rootReducer = combineReducers( {
        error: errorReducer,
        images: imagesReducers,
-       isLoading: loadingReducer
+       isLoading: loadingReducer,
+       nextPage: pageReducer
     });
 
 export default rootReducer;
